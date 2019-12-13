f7aa022d158015135d66382a6142b76c7f532a15
Cen3mode <40240844+Cen3mode@users.noreply.github.com>
2019-12-13 19:16:49 +0100

    002

--- flightControl.ino
+++ flightControl.ino
@@ -1,26 +1,160 @@
-#include <spi.h>
-#include <lora.h>
+// Feather9x_TX
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messaging client (transmitter)
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example Feather9x_RX
 
+#include <SPI.h>
+#include <RH_RF95.h>
+#include <BME280I2C.h>
+#include <Wire.h>
 
-void setup() {
-    Serial.begin(9600);
+// #define MYDEBUG 1
 
-    while(!Serial);
+//for feather m0
+#define RFM95_CS 53
+#define RFM95_RST 2
+#define RFM95_INT 3
 
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
 
-    while(!LoRa.begin(433000000)) {
-        Serial.println("Waiting for lora to start ...");
-        delay(100);
-    }
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+BME280I2C bme;
+
+void setup()
+{
+  Wire.begin();
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(1);
+  }
+
+  delay(100);
+
+  Serial.println("LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  delay(500);
+  
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+
+  while (!bme.begin())
+  {
+    Serial.println("Could not find BME280 sensor!");
+    delay(1000);
+  }
+
+  switch (bme.chipModel())
+  {
+    case BME280::ChipModel_BME280:
+      Serial.println("Found BME280 sensor! Success.");
+      break;
+    case BME280::ChipModel_BMP280:
+      Serial.println("Found BMP280 sensor! No Humidity available.");
+      break;
+    default:
+      Serial.println("Found UNKNOWN sensor! Error!");
+  }
 }
 
-void loop() {
-    LoRa.beginPacket();
-    LoRa.print("Hello, world");
-    LoRa.endPacket();
+int16_t packetnum = 0;  // packet counter, we increment per xmission
 
-    while(LoRa.isTransmitting()) {
-        delay(1);
+
+void loop()
+{
+  float temp(NAN), hum(NAN), pres(NAN);
+  BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+  BME280::PresUnit presUnit(BME280::PresUnit_Pa);
+  delay(1000); // Wait 1 second between transmits, could also 'sleep' here!
+  Serial.println("Transmitting..."); // Send a message to rf95_server
+
+  bme.read(pres, temp, hum, tempUnit, presUnit);
+
+  float radiopacket[2];
+  radiopacket[0] = temp;
+  radiopacket[1] = pres;
+
+  Serial.println(sizeof(float));
+  Serial.println(temp);
+  Serial.println(pres);
+
+  //int itemp = (int)(temp*100.0);
+  //int ipres = (int)(pres*100.0);
+
+  //sprintf(radiopacket, "%d, %d", ipres, itemp);
+
+  
+
+ // itoa(packetnum++, radiopacket + 13, 10);
+ // Serial.print("Sending ");  Serial.println(radiopacket);
+ // radiopacket[39] = 0;
+
+  Serial.println("Sending...");
+  delay(10);
+  rf95.send((uint8_t *)radiopacket, 2*sizeof(float));
+
+  Serial.println("Waiting for packet to complete...");
+  delay(10);
+  rf95.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+#if defined(MYDEBUG)
+
+  Serial.println("Waiting for reply...");
+  if (rf95.waitAvailableTimeout(1000))
+  {
+    // Should be a reply message for us now
+    if (rf95.recv(buf, &len))
+    {
+      Serial.print("Got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
     }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is there a listener around?");
+  }
+
+#endif
 
 }
