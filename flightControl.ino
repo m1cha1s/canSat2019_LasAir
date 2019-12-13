15fd9fa10bbcf2c018a3d5fa66583969ab166e38
Cen3mode <40240844+Cen3mode@users.noreply.github.com>
2019-12-13 17:44:53 +0100

    Added basic code test

--- flightControl.ino
+++ flightControl.ino
@@ -1,7 +1,26 @@
-void setup() {
+#include <spi.h>
+#include <lora.h>
 
+
+void setup() {
+    Serial.begin(9600);
+
+    while(!Serial);
+
+
+    while(!LoRa.begin(433000000)) {
+        Serial.println("Waiting for lora to start ...");
+        delay(100);
+    }
 }
 
 void loop() {
-    
+    LoRa.beginPacket();
+    LoRa.print("Hello, world");
+    LoRa.endPacket();
+
+    while(LoRa.isTransmitting()) {
+        delay(1);
+    }
+
 }
