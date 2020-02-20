d39cf55e760c8737635e285b8252bf6649a03fc8
Cen3mode <40240844+Cen3mode@users.noreply.github.com>
2020-02-20 17:50:04 +0100

    more data processing

--- backup/CanSat2019/DataProcessing/dataProcessing.py
+++ backup/CanSat2019/DataProcessing/dataProcessing.py
@@ -2,6 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = pd.read_csv("DATALOG.txt", header = None)
+labels = ["ID", "RSSI", "Time", "Temp1", "Temp2", "Pressure", "Lng", "Lat", "Speed", "Alt", "Sattelites", "SatValid", "AltValid", "LocValid", "PmValid", "PM 1.0", "PM 2.5", "PM 10.0", "n"]
 
-print(data)
+data = pd.read_csv("DATALOG.txt", sep=";", names = labels)
+data = data.drop("n", axis=1)
+#data = pd.DataFrame(data.sum(), columns = ["ID", "RSSI", "Time", "Temp1", "Temp2", "Pressure", "Lng", "Lat", "Speed", "Lat", "Sattelites", "SatValid", "AltValid", "LocValid", "PmValid", "PM 1.0", "PM 2.5", "PM 10.0"])
+
+print(data.head())
+
+for i in labels:
+    for x in range(2):
+        plt.plot(i, labels[x])
+        plt.xlabel(i)
+        plt.ylabel(labels[x])
+        plt.savefig("plots/"+i+"_"+labels[x]+"png")
